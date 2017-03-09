d8d5ca3ef166e9d13d5e1d9c1d181894305c5838
Kevin Nguyen <huykevin@gmail.com>
2017-03-08 23:25:09 -0800

    modify application java file

---
+++ src/main/java/clinic/programming/Application.java
@@ -0,0 +1,13 @@
+package clinic.programming;
+
+import java.util.*;
+
+public class Application {
+    public Application() {
+        System.out.println("Hello, inside");
+    }
+    public static void main(String[] args){
+        System.out.println("Hello");
+        Application app = new Application();
+    }
+}
